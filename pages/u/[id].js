fbbf84e3b6345f5c178252dbcecffe5f1bd7ecda
christianmancilla <44341625+christianmancilla@users.noreply.github.com>
2026-05-15 15:05:15 -0400

    Update [id].js

--- pages/u/[id].js
+++ pages/u/[id].js
@@ -60,6 +60,8 @@ export default function UserShare({ id, profile }) {
         />
 
         <meta property="og:image" content="https://share.jobin.app/jobin-share-og-v2.jpg" />
+        <meta property="og:image:width" content="670" />
+        <meta property="og:image:height" content="670" />
         <meta property="og:image:type" content="image/jpeg" />
         <meta property="og:type" content="website" />
         <meta
